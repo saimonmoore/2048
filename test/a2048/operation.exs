51fcf5a7b27321cae4b0e8b4d5e206cc3f01150a
Saimon Moore <saimonmoore@gmail.com>
2016-07-28 18:09:33 +0200

    Add shift left test

--- test/a2048/operation.exs
+++ test/a2048/operation.exs
@@ -2,7 +2,17 @@ defmodule A2048.OperationTest do
   use ExUnit.Case
   doctest A2048.Operation
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "#shift(:left) when no doubling" do
+    setup [:a_board_with_no_doubling]
+
+    test "shifts all numbers to the left" do
+      expected = [[2,0,0,0],[2,0,0,0],[2,4,0,0],[2,0,0,0]]
+      assert A2048.Operation.shift(:left, context[:board]) == expected
+    end
+  end
+
+  defp a_board_with_no_doubling do
+    board = [[0,0,2,0],[0,2,0,0],[2,4,0,0],[0,2,0,0]]
+    {board: board}
   end
 end
